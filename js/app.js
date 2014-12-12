448167414038606c47f8d2c6a1e80f8c50dc9ab1
javi <qualopec@gmail.com>
2014-12-12 19:05:35 +0100

    area

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var opensigpac = function() {
   });
 
   enableMobile(map);
+  areaControls(map);
 
   var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
---
+++ js/area.js
@@ -0,0 +1,32 @@
+
+
+function areaControls(map) {
+  var featureGroup = L.featureGroup().addTo(map);
+  var drawControl = new L.Control.Draw({
+    edit: {
+      featureGroup: featureGroup
+    },
+    draw: {
+      polygon: true,
+      polyline: false,
+      rectangle: false,
+      circle: false,
+      marker: false
+    }
+  }).addTo(map);
+
+  function showPolygonAreaEdited(e) {
+    e.layers.eachLayer(function(layer) {
+      showPolygonArea({ layer: layer });
+    });
+  }
+  function showPolygonArea(e) {
+    featureGroup.clearLayers();
+    featureGroup.addLayer(e.layer);
+    e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
+    e.layer.openPopup();
+  }
+
+  map.on('draw:created', showPolygonArea);
+  map.on('draw:edited', showPolygonAreaEdited);
+}
