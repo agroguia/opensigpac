56c7dddf363bd4024515e4b285468e1c55811779
Luis Bosque <luisico@gmail.com>
2014-12-12 19:57:15 +0100

    Upload usos fabric task

--- tools/proxy.py
+++ tools/proxy.py
@@ -18,10 +18,14 @@ def upload_conf():
   put('nginx/conf', '/etc/nginx/sites-available/opensigpac') 
   run("/etc/init.d/nginx restart")
 
+def upload_usos():
+  put('nginx/usos_sigpac.json', '/var/www/opensigpac/usos_sigpac.json') 
+
 def setup_nginx_server():
   run("apt-get install nginx -y")
   upload_conf()
   run("ln -s /etc/nginx/sites-available/opensigpac /etc/nginx/sites-enabled/opensigpac")
+  run("mkdir -p /var/www/opensigpac")
 
 def deploy_nginx_conf():
   upload_conf()
