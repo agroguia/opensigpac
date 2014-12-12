15537710bfa8281df4f2cb2b1e1ff8cab2aea273
javi <qualopec@gmail.com>
2014-12-12 16:24:08 +0100

    added interaction

--- js/plots_layer.js
+++ js/plots_layer.js
@@ -56,6 +56,20 @@ function PlotsLayer(map, tiledLayer, template) {
 }
 
 PlotsLayer.MAX_ZOOM = 14;
+PlotsLayer.DEFAULT_STYLE = {
+  fillOpacity: 0,
+  stroke: true,
+  weight: 1.5,
+  color: '#000'
+}
+
+PlotsLayer.DEFAULT_STYLE_HOVER = {
+  fillOpacity: 0.2,
+  fill: '#FFF',
+  stroke: true,
+  weight: 1.5,
+  color: '#000'
+}
 
 PlotsLayer.prototype = {
 
@@ -97,9 +111,29 @@ PlotsLayer.prototype = {
             });
           }
         }).addTo(self.map);
+        layerGeo.attributes = geo.Attributes;
+        layerGeo.id = geo.ID;
+        layerGeo.setStyle(PlotsLayer.DEFAULT_STYLE)
+        self.addInteraction(layerGeo);
         self.geometries[geo.ID] = layerGeo;
       }
     });
+  },
+
+  addInteraction: function(geo) {
+    geo.on('click', function() { 
+
+    })
+    geo.on('mouseover', function() { 
+      if (geo.hovered) return;
+      geo.setStyle(PlotsLayer.DEFAULT_STYLE_HOVER)
+      geo.hovered = true;
+    })
+    geo.on('mouseout', function() { 
+      if (!geo.hovered) return;
+      geo.hovered = false;
+      geo.setStyle(PlotsLayer.DEFAULT_STYLE)
+    })
   }
 
 
