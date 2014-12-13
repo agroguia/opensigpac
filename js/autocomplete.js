c0a311c99bf0b42532dbc756da264aa7257487b5
javi <qualopec@gmail.com>
2014-12-13 09:56:10 +0100

    hardcoded municipios

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -10,6 +10,25 @@ var parcelas = [];
 
 var selected_address;
 var selected_pol;
+var municipios = [];
+
+// this is called from data/municipios.js
+function opensigpac_municipio(data) {
+ _.map(data.rows, function(r) {   
+    municipios.push({
+      label: r.poblacion + " (" + r.provincia + ")",
+      value: r.poblacion,
+      meta:  {
+        poblacion: r.poblacion,
+        provincia: r.provincia,
+        lat: r.lat,
+        lng: r.lng,
+        id: r.id,
+        provincia_id: r.provincia_id
+      }
+    });     
+  });
+}
 
 
 function autocomplete(map) {
@@ -19,78 +38,62 @@ function autocomplete(map) {
 
     var sql = cartodb.SQL({ user: config.cartodb_user, completeDomain: config.sql_api });    
 
-    sql.execute(search_query).done(function(data) {
-           console.log(data.rows);    
+    //sql.execute(search_query).done(function(data) {
 
-           _.map(data.rows, function(r) {   
-              municipios.push({
-                label: r.poblacion + " (" + r.provincia + ")",
-                value: r.poblacion,
-                meta:  {
-                  poblacion: r.poblacion,
-                  provincia: r.provincia,
-                  lat: r.lat,
-                  lng: r.lng,
-                  id: r.id,
-                  provincia_id: r.provincia_id
-                }
-              });     
-            });
 
-          $("#localidad").autocomplete({
-            source: municipios,
-            minLength: 2,
-            select: function( event, ui ) {
-                selected_address = ui.item.meta;
-                var latlng = L.latLng(selected_address.lat, selected_address.lng);
-                map.panTo(latlng);
-                map.setZoom(16);
-                $('.Search-advanced').show();
-                console.log(selected_address);
+    $("#localidad").autocomplete({
+      source: municipios,
+      minLength: 2,
+      select: function( event, ui ) {
+          selected_address = ui.item.meta;
+          var latlng = L.latLng(selected_address.lat, selected_address.lng);
+          map.panTo(latlng);
+          map.setZoom(16);
+          $('.Search-advanced').show();
+          console.log(selected_address);
 
-                // TODO: Not always 0,0!!
-                var url = "http://opensigpac.cartodb.net/vectorsdg/query/poligonos/" + selected_address.provincia_id + "/" + selected_address.id + "/0/0.gzip"
-                
-                console.log("URL TO GET POLYGONS: " + url);
+          // TODO: Not always 0,0!!
+          var url = "http://opensigpac.cartodb.net/vectorsdg/query/poligonos/" + selected_address.provincia_id + "/" + selected_address.id + "/0/0.gzip"
+          
+          console.log("URL TO GET POLYGONS: " + url);
 
-                $.ajax({
-                    url: url, // 'test/15.15949.20555',
-                    type: "GET",
-                    //dataType: "binary",
-                    processData: false,
-                    success: function(data){
-                        data = Base64Binary.decodeArrayBuffer(data);
-                        
-                        var stuff = IO.BinaryGeometrySerializer.Read(data);
-                    
-                        
-                        for(var i = 0;i < stuff.length; i++) {
-                            console.log("POLYGON " + i + ":" + JSON.stringify(stuff[i]));
-                            
-                            poligonos.push({
-                                label: stuff[i].Attributes[0],
-                                value: stuff[i].Attributes[0]
-                            })
-                        }
-                        
-                        $("#poligono").autocomplete({
-                            source: poligonos,
-                            minLength: 1,
-                            select: function( event, ui ) {
-                                selected_pol = ui.item.value
-                                console.log("SELECTED POL: " + selected_pol)
-                            },
-                            position: {
-                                offset: "96 5"
-                            }
-                        })
-                    }
-                });
-            },
-            position: {
-                offset: "96 5"
-            }
+          $.ajax({
+              url: url, // 'test/15.15949.20555',
+              type: "GET",
+              //dataType: "binary",
+              processData: false,
+              success: function(data){
+                  data = Base64Binary.decodeArrayBuffer(data);
+                  
+                  var stuff = IO.BinaryGeometrySerializer.Read(data);
+              
+                  
+                  for(var i = 0;i < stuff.length; i++) {
+                      console.log("POLYGON " + i + ":" + JSON.stringify(stuff[i]));
+                      
+                      poligonos.push({
+                          label: stuff[i].Attributes[0],
+                          value: stuff[i].Attributes[0]
+                      })
+                  }
+                  
+                  $("#poligono").autocomplete({
+                      source: poligonos,
+                      minLength: 1,
+                      select: function( event, ui ) {
+                          selected_pol = ui.item.value
+                          console.log("SELECTED POL: " + selected_pol)
+                      },
+                      position: {
+                          offset: "96 5"
+                      }
+                  })
+              }
           });
-        });
+      },
+      position: {
+          offset: "96 5"
+      }
+    });
 }
 
